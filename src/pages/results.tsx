f8bae5fba827318397fefab673b8ac885fcde161
Misael Vazquez <misavazquezsifuentes@gmail.com>
2022-08-01 11:06:56 -0500

    Sort pokemons by percentage

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import { FC } from "react";
 import { AsyncReturnType } from "../utils/ts-bs";
 
+import { prisma } from "../server/db/client";
+
 const getPokemonInOrder = async () => {
-  return await prisma?.pokemon.findMany({
+  return await prisma.pokemon.findMany({
     orderBy: {
       VoteFor: { _count: "desc" },
     },
@@ -25,12 +27,12 @@ const getPokemonInOrder = async () => {
 type QueryPokemonResult = AsyncReturnType<typeof getPokemonInOrder>;
 
 const generateCountPercentage = (pokemon: QueryPokemonResult[number]) => {
-    const { VoteFor, VoteAgainst } = pokemon._count;
-    if (VoteFor + VoteAgainst === 0) {
-        return 0
-    }
-    return (VoteFor / (VoteFor + VoteAgainst)) * 100;
-}
+  const { VoteFor, VoteAgainst } = pokemon._count;
+  if (VoteFor + VoteAgainst === 0) {
+    return 0;
+  }
+  return (VoteFor / (VoteFor + VoteAgainst)) * 100;
+};
 
 const PokemonListing: FC<{ pokemon: QueryPokemonResult[number] }> = ({
   pokemon,
@@ -41,25 +43,29 @@ const PokemonListing: FC<{ pokemon: QueryPokemonResult[number] }> = ({
         <Image src={`${pokemon.spriteUrl}`} width={64} height={64} alt="" />
         <div className="capitalize">{pokemon.name}</div>
       </div>
-      <div className="pr-4">{generateCountPercentage(pokemon) + "%"}</div>
+      <div className="pr-4">{generateCountPercentage(pokemon).toFixed(2) + "%"}</div>
     </div>
   );
 };
 
-const results: FC<{ pokemon: QueryPokemonResult }> = (props) => {
+const Results: FC<{ pokemon: QueryPokemonResult }> = (props) => {
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-2xl p-4">Results</h2>
       <div className="flex flex-col border max-w-2xl w-full">
-        {props.pokemon?.map((pokemon, index) => (
-          <PokemonListing pokemon={pokemon} key={index} />
-        ))}
+        {props.pokemon
+          ?.sort(
+            (a, b) => generateCountPercentage(b) - generateCountPercentage(a)
+          )
+          .map((pokemon, index) => (
+            <PokemonListing pokemon={pokemon} key={index} />
+          ))}
       </div>
     </div>
   );
 };
 
-export default results;
+export default Results;
 
 export const getStaticProps: GetServerSideProps = async () => {
   const pokemonOrdered = await getPokemonInOrder();
